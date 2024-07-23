328ce1e1502b655d36c9674bdc2ea2a360b73a1f
fareeha1125 <fareeha1125@gmail.com>
2024-07-23 17:35:28 +0500

    update

---
+++ contracts/ERC721.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+import "@openzeppelin/contracts@5.0.2/utils/Strings.sol";
+
+contract CoreNFT is ERC721, ERC721URIStorage, Ownable {
+    using Strings for uint256;
+
+    uint256 private _nextTokenId;
+    string private _baseURIExtended;
+
+    constructor(string memory baseURI_)
+        ERC721("CoreNFT", "CORE")
+        Ownable(msg.sender)
+    {
+        _baseURIExtended = baseURI_;
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner {
+        _baseURIExtended = baseURI_;
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, string(abi.encodePacked(Strings.toString(tokenId), ".json")));
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIExtended;
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
+    }
+
+    function getCurrentTokenId() public view returns (uint256) {
+        return _nextTokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+}
